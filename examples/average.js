9fb207f7df3047780054ee4e82c92a90e3394738
talkquazi <talkquazi@gmail.com>
2016-08-18 11:56:38 -0400

    push to version 1.0.0

--- examples/average.js
+++ examples/average.js
@@ -1,12 +1,12 @@
 var hind = require('../lib/hind.js');
 
 function testFunction(data, datab) {
-	console.log('I am testFunction', data, datab);
+  console.log('I am testFunction', data, datab);
 }
 
 testFunction = hind(testFunction, function(func, argz, argy) {
-	func(argz, argy);
-	console.log('This was called after testFunction');
+  func(argz, argy);
+  console.log('This was called after testFunction');
 });
 
 testFunction("Hello", "World");
--- examples/simple.js
+++ examples/simple.js
@@ -1,12 +1,12 @@
 var hind = require('../lib/hind.js');
 
 function testFunction() {
-	console.log('I am testFunction');
+  console.log('I am testFunction');
 }
 
 testFunction = hind(testFunction, function(func) {
-	func();
-	console.log('This was called after testFunction');
+  func();
+  console.log('This was called after testFunction');
 });
 
 testFunction();
